f769aa2a48a1f2854a5082a59fcbc723fa74c744
Marc <marc.solis@enti.cat>
2020-11-11 11:23:06 +0100

    MVC
    Simple Shop Buttons System

---
+++ UnitySolution/Assets/Scripts/Core/Shop/ShopMediator.cs
@@ -0,0 +1,36 @@
+﻿using UniRx;
+
+namespace MarcTest.Shop
+{
+    public class ShopMediator
+    {
+        public ShopMediator(ShopView view, ShopModel model)
+        {
+            //Initial State
+            view.Init(model.Character1Price.Value, model.Character2Price.Value);
+
+            //Suscriptions
+            view.Char1ButtonClickedEvent.AsObservable().Subscribe(e =>
+            {
+                model.Character1Price.Value++;
+            });
+
+            model.Character1Price.AsObservable().Subscribe(value =>
+            {
+                view.SetChar1Price(value);
+            });
+
+            view.Char2ButtonClickedEvent.AsObservable().Subscribe(e => 
+            {
+                model.Character2Price.Value++;
+            });
+
+            model.Character2Price.AsObservable().Subscribe(value =>
+            {
+                view.SetChar2Price(value);
+            });
+
+        }
+    }
+}
+
---
+++ UnitySolution/Assets/Scripts/Core/Shop/ShopModel.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+
+namespace MarcTest.Shop
+{
+    public class ShopModel
+    {
+        public ReactiveProperty<int> Character1Price, Character2Price;
+
+        public ShopModel()
+        {
+            Character1Price = new ReactiveProperty<int>(10);
+            Character2Price = new ReactiveProperty<int>(20);
+        }
+
+
+    }
+}
+
